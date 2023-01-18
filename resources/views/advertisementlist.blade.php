1edce799cca5f76c919b547d78cc8c2031805b1e
Mykolas <mykolas.kucinskas2@gmail.com>
2023-01-18 17:52:21 +0200

    created a blade view

---
+++ resources/views/advertisementlist.blade.php
@@ -0,0 +1,6 @@
+
+@extends('homepage')
+
+@section('advertisementlist')
+    <p>This is some content over here.</p>
+@endsection
---
+++ resources/views/homepage.blade.php
@@ -0,0 +1,5 @@
+
+<h1>ThingsAreForSale</h1>
+<p>On this page you can buy and sell things you don't need anymore!</p>
+
+@yield('advertisementlist', 'This is alternative')
